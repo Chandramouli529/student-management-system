0f77f8b77d14d9f733ff557411d90078e7c5000e
Chandramouli529 <chandramouli@fusionevalx.ai>
2026-07-03 16:43:13 +0530

    path changed

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,8 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import { AuthProvider } from "./context/AuthContext.jsx";
 import { DirectoryProvider } from "./context/DirectoryContext.jsx";
-import "./styles/global.css";
+import "./styles/globals.css";
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
